77f359f41e35860cef96b270a75468a4ced249b4
qquk <qquk@mail.ru>
2019-10-03 13:49:01 +0300

    hw2

---
+++ HW2/objects/app.js
@@ -0,0 +1,14 @@
+// 1. Создать объект с полем product, равным ‘iphone’
+// 2. Добавить в объект поле price, равное 1000 и поле currency, равное ‘dollar’
+// 3. Добавить поле details, которое будет содержать объект с полями model и color
+
+let phone = {
+    product : 'iphone'
+}
+
+phone.price = 1000;
+phone.currency = 'dollar';
+phone.details = {
+    model: 'X',
+    color: 'silver'
+}
